7c636c6c17092344d4783fd27718d6ee2573cd2e
Shyotl <Shyotl@gmail.com>
2015-01-29 17:49:18 -0600

    Remove unused linear_to_srgb function from several shaders.

--- indra/newview/app_settings/shaders/class1/deferred/multiSpotLightF.glsl
+++ indra/newview/app_settings/shaders/class1/deferred/multiSpotLightF.glsl
@@ -102,26 +102,6 @@ vec3 srgb_to_linear(vec3 cs)
 
 }
 
-vec3 linear_to_srgb(vec3 cl)
-{
-	cl = clamp(cl, vec3(0), vec3(1));
-	vec3 low_range  = cl * 12.92;
-	vec3 high_range = 1.055 * pow(cl, vec3(0.41666)) - 0.055;
-	bvec3 lt = lessThan(cl,vec3(0.0031308));
-
-#ifdef OLD_SELECT
-	vec3 result;
-	result.r = lt.r ? low_range.r : high_range.r;
-	result.g = lt.g ? low_range.g : high_range.g;
-	result.b = lt.b ? low_range.b : high_range.b;
-    return result;
-#else
-	return mix(high_range, low_range, lt);
-#endif
-
-}
-
-
 vec4 texture2DLodSpecular(sampler2D projectionMap, vec2 tc, float lod)
 {
 	vec4 ret = texture2DLod(projectionMap, tc, lod);
