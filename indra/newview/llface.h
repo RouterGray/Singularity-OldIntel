18c6330af79a1c1bdc6becc4244b5b01702bbf66
Shyotl <Shyotl@gmail.com>
2015-06-25 20:24:25 -0500

    Remove unused index_array pointer in LLFace::renderElements and LLFace::pushVertices

--- indra/newview/llface.h
+++ indra/newview/llface.h
@@ -136,10 +136,10 @@ public:
 	void            setIndexInTex(U32 ch, S32 index) { llassert(ch < LLRender::NUM_TEXTURE_CHANNELS);  mIndexInTex[ch] = index ;}
 
 	void			renderSetColor() const;
-	S32				renderElements(const U16 *index_array) const;
+	S32				renderElements() const;
 	S32				renderIndexed ();
 	S32				renderIndexed (U32 mask);
-	S32				pushVertices(const U16* index_array) const;
+	S32				pushVertices() const;
 	
 	void			setWorldMatrix(const LLMatrix4& mat);
 	const LLTextureEntry* getTextureEntry()	const { return mVObjp->getTE(mTEOffset); }
