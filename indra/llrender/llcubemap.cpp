3fc035f5681809a475ec6c89cf8fb32fa83f71e3
Shyotl <Shyotl@gmail.com>
2015-07-23 01:30:31 -0500

    Trivial cleanup of removal of translation component from transformation matrix when drawing cubemaps.

--- indra/llrender/llcubemap.cpp
+++ indra/llrender/llcubemap.cpp
@@ -266,14 +266,8 @@ void LLCubeMap::setMatrix(S32 stage)
 		gGL.getTexUnit(stage)->activate();
 	}
 
-	LLVector3 x(gGLModelView.getRow<0>().getF32ptr());
-	LLVector3 y(gGLModelView.getRow<1>().getF32ptr());
-	LLVector3 z(gGLModelView.getRow<2>().getF32ptr());
-
-	LLMatrix3 mat3;
-	mat3.setRows(x,y,z);
-	LLMatrix4a trans;
-	trans.loadu(mat3);
+	LLMatrix4a trans(gGLModelView);
+	trans.setRow<3>(LLVector4a::getZero());
 	trans.transpose();
 
 	gGL.matrixMode(LLRender::MM_TEXTURE);
